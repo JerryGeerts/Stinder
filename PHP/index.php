c2e2e05867838893b32e74580513327c870dbf0a
Jerry Geerts <jerry8@live.nl>
2017-08-28 23:58:32 +0200

    Removing fun

--- PHP/index.php
+++ PHP/index.php
@@ -12,7 +12,6 @@
     			<h1>Intern</h1>
     			<h2><?php echo $error; ?></h2>
     			<h2>Vind een stage plek op een leuke manier!</h2>
-    		<!-- 	<p>Deze website gaat je helpen om een stage te vinden in no time!<br> Zolang je ons vertrouwt met je persoonlijke gegevens komt het helemaal goed xoxo.</p> -->
     			<a class="btn" href="https://sappigejongens.nl/stinder/PHP/register.php?type=company">Registreer als bedrijf!</a>	
     			<a class="btn" href="https://sappigejongens.nl/stinder/PHP/register.php">Registreer als leerling!</a>
 			</div>
@@ -23,29 +22,29 @@
  	<div class="row about">
     	<div class="col-lg-4" >
     		<div>	
-    			<h3>Over Intern</h3>
-    			<p>Intern is een web applicatie waarmee je in contact kan komen met misschien een toekomstig stage bedrijf.<br>Wij zonderen ons af van websites zoals Stagemarkt.nl omdat je bij ons een account het met de soort stage die je zoekt en je word gematched met een bedrijf als een bedrijf interesse in jou heeft. <br> Op deze manier weet je al dat er een bedrijf is die waarschijnlijk perfect voor jou is. </p>
+    			<h3>Lorem ipsum</h3>
+    			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sagittis turpis in diam sollicitudin, sed ornare diam pretium. Aenean finibus, .<br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sagittis turpis in diam sollicitudin, sed ornare diam pretium. Aenean finibus, . <br> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sagittis turpis in diam sollicitudin, sed ornare diam pretium. Aenean finibus, . </p>
     		</div>
     	</div>
 
     	<div class="col-lg-4" >
     		<div>
-    			<h3>Over Dusty</h3>
-    			<p>Dusty weet niet zo goed wat hij moet met ze leven(Same stiekem), hij heeft volgens mij 2/3 van zijn tijd zitten nadenken over wat hij uberhaubt met ze leven doet.<br><br> Dit vind ik mooi aan Dusty, het enige wat niet zo mooi aan hem is, is wanneer hij flext dan word ik een beetje jaloers en dat weet hij zelf ook donders goed xoxoxo. Ik moet meer shit zeggen om dit op te vullen.</p>
+    			<h3>Lorem ipsum</h3>
+    			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sagittis turpis in diam sollicitudin, sed ornare diam pretium. Aenean finibus, .<br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sagittis turpis in diam sollicitudin, sed ornare diam pretium. Aenean finibus, . <br> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sagittis turpis in diam sollicitudin, sed ornare diam pretium. Aenean finibus, . </p>
     		</div>
     	</div>
 
     	<div class="col-lg-4" >
     		<div>
-    			<h3>Over ons</h3>
-    			<p>Wij zijn een groep van 3 studenten die vrij weinig te doen hebben dus maar deze website maken in hun vrije tijd<br> Wij doen niet aan documentatie en op die moment weet ik niet zo goed wat ik hier nog kan zetten dus ik tik een beetje door yo klik <a href="">hier!</a> <br><br> Je kan daar meer over ons te weten komen als je wilt natuurlijk ik force je niet ofzo maar ik heb wel me best gedaan. </p>
+    			<h3>Lorem ipsum</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sagittis turpis in diam sollicitudin, sed ornare diam pretium. Aenean finibus, .<br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sagittis turpis in diam sollicitudin, sed ornare diam pretium. Aenean finibus, . <br> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sagittis turpis in diam sollicitudin, sed ornare diam pretium. Aenean finibus, . </p>
     		</div>
     	</div>
  	</div>
 
  	<div class="row quote">
     	<div class="col-lg-12">
-    		<p>'Wij flexen op de beat van het leven'</p>
+    		<p>'Lorem ipsum Suspendisse '</p>
       	</div>
  	</div>
 
